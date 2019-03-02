ea48c97148ea1a065572e31e897bb4862805edd1
surya karri <karri.surya5@gmail.com>
2019-03-02 21:00:10 +0700

    Add ignorefile

--- CouponMVC/CouponMVC/Models/Finalprice.cs
+++
@@ -1,9 +0,0 @@
-namespace CouponMVC.Models
-{
-    public class FinalPrice
-    {
-        public int totalprice {get; set;}
-        public int price {get;set;}
-        public int discountprice {get; set;}
-    }
-}
